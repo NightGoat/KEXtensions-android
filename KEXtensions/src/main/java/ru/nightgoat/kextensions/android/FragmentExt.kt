2c8e63647e4be493431eb63625db57f6689a6f40
Nail Asadullin <alxumeg@gmail.com>
2021-07-04 17:19:17 +0300

    added Fragment.argument

--- KEXtensions/src/main/java/ru/nightgoat/kextensions/android/FragmentExt.kt
+++ KEXtensions/src/main/java/ru/nightgoat/kextensions/android/FragmentExt.kt
@@ -1,9 +1,11 @@
 package ru.nightgoat.kextensions.android
 
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import ru.nightgoat.kextensions.unsafeLazy
 
 fun <T : ViewModel> Fragment.provideViewModel(clazz: Class<T>): T {
     return ViewModelProvider(this).get(clazz)
@@ -15,4 +17,24 @@ fun <T> Fragment.wakeUpLiveData(liveData: LiveData<T>) {
 
 fun <T> Fragment.wakeUpLiveData(listOfLD: List<LiveData<T>>) {
     listOfLD.forEach(::wakeUpLiveData)
+}
+
+inline fun <reified T> Fragment.argument(argumentKey: String): Lazy<T?> = unsafeLazy {
+    arguments?.get(argumentKey) as? T
+}
+
+inline fun <reified T> DialogFragment.argument(argumentKey: String): Lazy<T?> = unsafeLazy {
+    arguments?.get(argumentKey) as? T
+}
+
+inline fun <reified T> Fragment.argumentOrDefault(argumentKey: String, defaultValue: T): Lazy<T> =
+    unsafeLazy {
+        arguments?.get(argumentKey) as? T ?: defaultValue
+    }
+
+inline fun <reified T> DialogFragment.argumentOrDefault(
+    argumentKey: String,
+    defaultValue: T
+): Lazy<T> = unsafeLazy {
+    arguments?.get(argumentKey) as? T ?: defaultValue
 }
